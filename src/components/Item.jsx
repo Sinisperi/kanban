1e2e40e0938e531fbf6c8ec344746cf51639278a
sinisperi <sinisperi@gmail.com>
2023-08-24 22:23:40 +0300

    separated reducer to a new file

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 export const Item = ({ data, origin, setPlaceAfter }) => {
   const [isSomeoneInsideMe, setIsSomeoneInsideMe] = React.useState(false);
+  const [isBeingDragged, setBeingDragged] = React.useState(false);
   const handleDragStart = (e) => {
     e.dataTransfer.setData("id", data.id);
     e.dataTransfer.setData("origin", origin);
+    setBeingDragged(() => true);
     e.stopPropagation();
   };
 
@@ -13,13 +15,21 @@ export const Item = ({ data, origin, setPlaceAfter }) => {
   };
   const handleDrop = () => {
     setIsSomeoneInsideMe(() => false);
+    setBeingDragged(() => false);
   };
   const handleDragLeave = () => {
     setIsSomeoneInsideMe(() => false);
   };
+
   return (
     <h3
-      style={{ borderTop: isSomeoneInsideMe ? "1px solid red" : "none" }}
+      className="item"
+      style={{
+        boxShadow: isSomeoneInsideMe
+          ? "0px -2px 0.7px -0.4px rgba(33,156,194,1)"
+          : "none",
+        // opacity: isBeingDragged ? 0.5 : 1,
+      }}
       draggable
       onDragEnter={handleDragEnter}
       onDragLeave={handleDragLeave}
---
+++ src/context/reducer.js
@@ -0,0 +1,76 @@
+import { Actions } from "./actions";
+export const InitState = {};
+
+const indOf = (id, arr) => {
+  let ind = 0;
+  for (let i of arr) {
+    if (i.id === id) return ind;
+    ind++;
+  }
+  return undefined;
+};
+
+export const RootReducer = (state = InitState, action) => {
+  switch (action.type) {
+    case Actions.CREATE_COLUMN: {
+      if (Object.keys(state).indexOf(action.payload.name) >= 0) {
+        return state;
+      }
+      return { ...state, [action.payload.name]: [] };
+    }
+    case Actions.CREATE_ITEM: {
+      const newColumn = state[action.payload.destination];
+      if (!newColumn) return state;
+      newColumn.push(action.payload.data);
+      return {
+        ...state,
+        [action.payload.destination]: newColumn,
+      };
+    }
+    case Actions.DROP_ITEM: {
+      const itemToMove = state[action.payload.origin].filter(
+        (i) => i.id === action.payload.id
+      )[0];
+      if (
+        state[action.payload.destination].filter(
+          (i) => i.id === action.payload.id
+        )[0]
+      ) {
+        const arr = state[action.payload.destination];
+        let arrWithout;
+        let arrWith;
+
+        const moveId = indOf(itemToMove.id, arr);
+        const moveAfterId = indOf(action.payload.placeAfter, arr);
+        arrWithout = arr.toSpliced(moveId, 1);
+        arrWith = arrWithout.toSpliced(moveAfterId, 0, itemToMove);
+        return { ...state, [action.payload.destination]: arrWith };
+      }
+      const fromArray = state[action.payload.origin].filter(
+        (i) => i.id !== action.payload.id
+      );
+      const toArray = state[action.payload.destination];
+
+      const moveAfterId = indOf(action.payload.placeAfter, toArray);
+      if (moveAfterId === undefined) {
+        toArray.push(itemToMove);
+        return {
+          ...state,
+          [action.payload.origin]: fromArray,
+          [action.payload.destination]: toArray,
+        };
+      }
+
+      const newArr = toArray.toSpliced(moveAfterId, 0, itemToMove);
+
+      return {
+        ...state,
+        [action.payload.origin]: fromArray,
+        [action.payload.destination]: newArr,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
